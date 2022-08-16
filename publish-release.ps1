df7ea4fef89e9e8e36d625957f37d55eb426dce3
SGrottel <sgrottel@sgrottel.de>
2022-08-16 08:22:34 +0200

    Migrated to Github Actions

--- publish-release.ps1
+++ publish-release.ps1
@@ -8,5 +8,5 @@ dotnet publish -c release /p:CopyOutputSymbolsToPublishDirectory=false
 rm ".\bin\Release\*.zip" -ErrorAction Ignore
 rm ".\bin\Release\Redate" -recurse -force -ErrorAction Ignore
 mkdir ".\bin\Release\Redate"
-copy ".\bin\Release\net5.0\publish\*" ".\bin\Release\Redate" -recurse -force
+copy ".\bin\Release\net6.0\publish\*" ".\bin\Release\Redate" -recurse -force
 Compress-Archive -Path ".\bin\Release\Redate" -DestinationPath (".\bin\Release\Redate-" + $version + ".zip") -force
